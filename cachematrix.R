8bb4e7e3326fa92114c5af8258899472707f24c2
TrishP <Trish_P@outlook.com>
2016-02-18 09:30:26 -0800

    Updates for Assignment 2, Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions which
+##     get the original matrix
+##     set the original matrix
+##     get the inverse of the matrix
+##     set the inverse of the matrix
+## makeCacheMatrix takes in the original matrix and stores it
+
+## Usage example:
+##   w <- matrix(1:4, ncol=2)
+##   m <- makeCacheMatrix(w)
+##   m$get()
+##   cacheSolve(m)
+##   m$getinvrs()
 
 makeCacheMatrix <- function(x = matrix()) {
+     invrs <- NULL
+     set <- function(y) {
+          x <<- y
+          invrs <<- NULL
+     }
+     get <- function() x
+     setinvrs <- function(minvrs) invrs <<- minvrs
+     getinvrs <- function() invrs
+     list(set = set, get = get,
+          setinvrs = setinvrs,
+          getinvrs = getinvrs)
+     
+}     
 
-}
 
-
-## Write a short comment describing this function
+## cacheSolve either retrieves the matrix inverse from cache 
+##      or calculates it and stores it in the cache  
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     invrs <- x$getinvrs()
+     if(!is.null(invrs)) {
+          message("getting cached data")
+          return(invrs)
+     }
+     data <- x$get()
+     invrs <- solve(data, ...)
+     x$setinvrs(invrs)
+     invrs
 }
+
